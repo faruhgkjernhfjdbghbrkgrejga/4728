6d2574cc7c9503a31256483841d4b602336995e4
faruhgkjernhfjdbghbrkgrejga <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-10 19:04:56 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# Install Tesseract OCR
+# Install system dependencies
 apt-get update
+apt-get install -y libsm6 libxext6 libxrender-dev
+
+# Install Python dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Install Tesseract OCR
 apt-get install -y tesseract-ocr
 
 # Install English language data
